1d071f70023d236149fb23dd36273163a7d5b0ff
Lakshay_Pratap_Singh <pratapsinghlakshay8@gmail.com>
2025-03-19 16:28:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import LoginSignup from "./Components/LoginSignup/LoginSignup";
+function App() {
+  return (
+    <>
+      <div>
+        <LoginSignup></LoginSignup>
+      </div>
+    </>
+  );
+}
+
+export default App;
